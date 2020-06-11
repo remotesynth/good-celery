4a4503da590369682aa3d195836bd4428c74b26d
stackbit-projects <projects@stackbit.com>
2020-06-11 18:00:13 +0000

    Update configuration

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ Stackbit Azimuth Theme original README is located [here](./README.theme.md).
 
 1. run the following command to fetch additional site contents from Stackbit if needed:
 
-        npx @stackbit/stackbit-pull --stackbit-pull-api-url=__STACKBIT_PULL_API_URL__
+        npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5ee271200d08e6001bd3b628
 
 1. Starts a development server
 
--- stackbit-build.sh
+++ stackbit-build.sh
@@ -4,12 +4,12 @@ set -e
 set -o pipefail
 set -v
 
-curl -s -X POST __STACKBIT_WEBHOOK_URL__/pull > /dev/null
+curl -s -X POST https://api.stackbit.com/project/5ee271200d08e6001bd3b628/webhook/build/pull > /dev/null
 if [[ -z "${STACKBIT_API_KEY}" ]]; then
     echo "WARNING: No STACKBIT_API_KEY environment variable set, skipping stackbit-pull"
 else
-    npx @stackbit/stackbit-pull --stackbit-pull-api-url=__STACKBIT_PULL_API_URL__ 
+    npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5ee271200d08e6001bd3b628 
 fi
-curl -s -X POST __STACKBIT_WEBHOOK_URL__/ssgbuild > /dev/null
+curl -s -X POST https://api.stackbit.com/project/5ee271200d08e6001bd3b628/webhook/build/ssgbuild > /dev/null
 gatsby build
-curl -s -X POST __STACKBIT_WEBHOOK_URL__/publish > /dev/null
+curl -s -X POST https://api.stackbit.com/project/5ee271200d08e6001bd3b628/webhook/build/publish > /dev/null
